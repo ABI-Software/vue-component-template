50d8702e4a57892d95ac10094786b68067d52aa1
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-06-07 10:22:04 +1200

    Update component testing

--- cypress/component/VueComponentTemplate.cy.js
+++ cypress/component/VueComponentTemplate.cy.js
@@ -1,8 +1,21 @@
 import VueComponentTemplate from '../../src/components/VueComponentTemplate.vue';
 
+const TITLE = 'Vue Component Template';
+
 describe('<VueComponentTemplate />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(VueComponentTemplate)
+  });
+
+  it(`should the title: ${TITLE}`, () => {
+    cy.mount(VueComponentTemplate, {
+      props: {
+        title: TITLE
+      }
+    })
+    .then(() => {
+      cy.get('h1').should('have.text', TITLE);
+    });
   });
 });
